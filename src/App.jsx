b2e3e58cff304f70e4aaba8d401171e84af33862
Daniel Salgado <daniel.salgado@silice.si>
2023-08-23 02:40:18 -0500

    Correccion a import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
